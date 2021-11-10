d843a8c5ea5259b83248435016f8eccd1c12702f
Grace Long <glong@mail.umw.edu>
2021-11-10 09:21:23 -0500

    add search by location function

--- backend/postings.js
+++ backend/postings.js
@@ -1,6 +1,7 @@
 module.exports = {
     addPost,
     searchPosts,
+    searchPostsLocation,
     getApprovedPosts,
     removePost,
     getUnapprovedPosts,
@@ -11,6 +12,18 @@ module.exports = {
 async function searchPosts(pool, q){
     try{
         const template = `SELECT * FROM postings WHERE title || ' ' || company_name || ' ' || description ILIKE '%${q}%';`;
+        const res = await pool.query(template);
+        console.log(res.rows);
+        return res.rows;
+    }catch(err){
+        console.log(err.stack);
+        return err;
+    }
+}
+
+async function searchPostsLocation(pool, q){
+    try{
+        const template = `SELECT * FROM postings WHERE city || ' ' || state ILIKE '%${q}%';`;
         const res = await pool.query(template);
         console.log(res.rows);
         return res.rows;
